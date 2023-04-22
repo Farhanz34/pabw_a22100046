82eadddb84fcd482f17d4eb6df26c4bdf67383dd
Farhanz34 <farhanzafrizalm250403@gmail.com>
2023-04-22 13:59:53 +0700

    menambahkan fitur insert data pada database

--- kuliah/pertemuan7/functions.php
+++ kuliah/pertemuan7/functions.php
@@ -23,7 +23,21 @@ function query($query) {
   return $rows;
 }
 
+function tambah($data){
 
+  $conn = koneksi();
+
+  $nama = htmlspecialchars($data['nama']); //htmlspecialchars berfungsi agar semua data yang dikirim user di periksa (untuk menghindari hacking source code)
+  $nim = htmlspecialchars($data['nim']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "INSERT INTO mahasiswa VALUES (null, '$nama', '$nim', '$email', '$jurusan', '$gambar');"; // Insert into dipecah / bisa disatukan dengan $koneksi
+  mysqli_query($conn, $query);
+  echo mysqli_error($conn); // Untuk mengetahui letak kesalahan pada saat menambahkan data
+  return mysqli_affected_rows($conn); // Untuk memberitahu data pada database apakah ada baris yang bertambah, hilang, dihapus atau di edit
+}
 
 
 ?>
